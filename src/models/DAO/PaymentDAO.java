556110f2b8a8f2587821d753d2685cb005088157
moad91 <ezzaibouh.moad@inemail.ine.inpt.ma>
2022-06-12 21:51:58 +0100

    Sprint5-Gestion des Paiement

---
+++ src/models/DAO/PaymentDAO.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models.dao;
+
+import database.Database;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+import models.PatientInfo;
+import models.Payment;
+
+/**
+ *
+ * @author otsaan
+ */
+public class PaymentDAO implements DAO<Payment> {
+    
+    
+    @Override
+    public Payment find(String id) {
+        
+        Payment payment = new Payment();
+        PatientDAO patientDAO = DAOFactory.getPatientDAO();
+        
+        String findQuery = "SELECT * "
+                        + "FROM paiements "
+                        + "WHERE id_paiement = " + Integer.parseInt(id) + ";";
+
+        ResultSet rs = database.Database.getInstance().query(findQuery);
+            
+        try {
+            
+            rs.first();
+            payment.setId(Integer.parseInt(id));
+            payment.setAmount(rs.getDouble("montant"));
+            payment.setDate(rs.getDate("date_paiement"));
+            payment.setPatient(patientDAO.find(rs.getString(id)));
+            
+        } catch (SQLException ex) {
+            System.out.println("Error : PaymentDAO.find()" + ex);
+        }
+        
+        return payment;
+    }
+
+
+    @Override
+    public boolean create(Payment p) {
+        
+        String insertQuery = "INSERT INTO paiements(montant, date_paiement, id_patient) VALUES("
+                           + p.getAmount() + ", "
+                           + "now(), "
+                           + p.getPatient().getPatientId() + ");";
+        
+        p.setId(Database.getInstance().dmlQuery2(insertQuery));
+        return (p.getId()!=0);
+    }
+
+    
+    @Override
+    public boolean update(Payment p) {
+        
+        String updateQuery = "UPDATE paiements "
+                          + "SET montant = " + p.getAmount()+ ", "
+                          + "date_paiement = '" + p.getDate()+ "', "
+                          + "id_patient = " + p.getPatient().getPatientId() + " "
+                          + "WHERE id_paiement = " + p.getId() + ";";
+        
+        return (Database.getInstance().dmlQuery(updateQuery) != 0);
+    }
+
+    
+    @Override
+    public boolean delete(Payment p) {
+        
+        String deleteQuery = "DELETE FROM paiements "
+                          + "WHERE id_paiement = " + p.getId() + ";";
+        
+        return (Database.getInstance().dmlQuery(deleteQuery) != 0);
+    }
+    
+    
+    @Override
+    public Vector<Payment> all() {
+        
+        Vector<Payment> payments = new Vector<Payment>();
+        PatientDAO patientDAO = DAOFactory.getPatientDAO();
+        
+
+        String findQuery = "SELECT * "
+                        + "FROM paiements order by date_paiement desc;";
+        
+        ResultSet rs = database.Database.getInstance().query(findQuery);
+
+        try {
+            
+            while(rs.next()) {
+                
+                Payment p = new Payment();
+                
+                p.setId(rs.getInt("id_paiement"));
+                p.setAmount(rs.getDouble("montant"));
+                p.setDate(rs.getDate("date_paiement"));
+                p.setPatient(patientDAO.find(rs.getString("id_patient")));
+            
+                payments.add(p);
+            }
+            
+        } catch (SQLException ex) {
+            System.out.println("Error : PaymentDAO.all()" + ex);
+        }
+        
+        return payments;
+    }
+    
+    
+}
---
+++ src/models/Payment.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author otsaan
+ */
+public class Payment {
+    
+    private int id;
+    private double amount;
+    private Date date;
+    private Patient patient;
+
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" + "id=" + id + ", amount=" + amount + ", date=" + date + ", patient=" + patient + '}';
+    }
+    
+    
+}
